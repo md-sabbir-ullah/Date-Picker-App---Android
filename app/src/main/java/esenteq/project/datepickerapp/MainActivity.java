39bd94be7a80b51b2647d85787336c4d011f3ece
DESKTOP-NB4VQMM\USER <mullah152144@bscse.uiu.ac.bd>
2020-04-08 16:58:55 +0600

    initial upload

---
+++ app/src/main/java/esenteq/project/datepickerapp/MainActivity.java
@@ -0,0 +1,53 @@
+package esenteq.project.datepickerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView setDate;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializer();
+        setUserDOB();
+    }
+
+    private void setUserDOB() {
+        setDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //using date-picker-dialog:
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this,
+                        android.R.style.Theme_DeviceDefault_Dialog_MinWidth, dateSetListener,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                setDate.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+            }
+        };
+    }
+
+    private void initializer() {
+        setDate = findViewById(R.id.tv_setdateID);
+    }
+}
